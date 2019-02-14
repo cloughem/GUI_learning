bef7b8e46c60e3b2fdb9fb03a2bbfe069e0b01e9
cloughem <cloughem@wgc.school.nz>
2019-02-15 10:10:22 +1300

    commit 2

--- Hello World.py
+++ Hello World.py
@@ -5,7 +5,15 @@ root.title('Hewwo wowd')  # sets the title of the window
 root.geometry("600x200")  # sets the window's width and height
 
 my_label = tkinter.Label(root)
-my_label.config.text="Hewwo UwU", fg="#ff69b4" font=("windings")
+my_label.config(text="Hewwo UwU", fg="#ff69b4", bg="pink", font=("", "20"))
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Welcome to hell", fg="#ff69b4", bg="purple")
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="You're trapped", fg="#ff69b4", bg="blue",  font=("Comic Sans ms", "50"), relief="ridge")
 my_label.grid()
 
 root.mainloop()
