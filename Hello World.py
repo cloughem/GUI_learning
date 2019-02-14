8809b57201b6321fcfef709d540a4ebc4ca9d20d
cloughem <cloughem@wgc.school.nz>
2019-02-14 15:17:46 +1300

    Label

--- Hello World.py
+++ Hello World.py
@@ -1,3 +1,11 @@
 import tkinter
 root = tkinter.Tk()
+
+root.title('Hewwo wowd')  # sets the title of the window
+root.geometry("600x200")  # sets the window's width and height
+
+my_label = tkinter.Label(root)
+my_label.config.text="Hewwo UwU", fg="#ff69b4" font=("windings")
+my_label.grid()
+
 root.mainloop()
