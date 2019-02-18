50d6808499bd32a5a163dd0f5a32070ba9aca0c3
cloughem <cloughem@wgc.school.nz>
2019-02-19 10:01:18 +1300

    changed tkinter import

--- button count.py
+++ button count.py
@@ -1,4 +1,4 @@
-import tkinter
+from tkinter import *
 
 count = 0
 
@@ -6,23 +6,30 @@ count = 0
 def count_up():
     global count
     count += 1
-    print(count)
+    count_text.set(count)
 
 
 def count_down():
     global count
     count -= 1
-    print(count)
+    count_text.set(count)
 
 
-root = tkinter.Tk()
+root = Tk()
 
-up_btn = tkinter.Button(root)
-up_btn.config(text="Count up", bg="black", fg="green", command=count_up)
+count_text = StringVar()
+count_text.set("0")
+
+up_btn = Button(root)
+up_btn.config(text="Count up", bg="black", fg="green", font="wingdings", command=count_up)
 up_btn.grid()
 
-down_btn = tkinter.Button(root)
-down_btn.config(text="Count down", command=count_down)
+down_btn = Button(root)
+down_btn.config(text="Count down", bg="black", fg="pink", font="wingdings", command=count_down)
 down_btn.grid()
+
+count_label = Label(root)
+count_label.config(textvariable=count_text)
+count_label.grid()
 
 root.mainloop()
