b53f9f035b136942cfe02e7bfce94d4c48057c00
cloughem <cloughem@wgc.school.nz>
2019-02-20 14:31:27 +1300

    Calcul8 step 1

--- Calcul8.py
+++ Calcul8.py
@@ -3,4 +3,18 @@ from tkinter import *
 root = Tk()
 root.title("Calcul8")
 
+
+input_text = StringVar()
+input_text.set("")
+
+answer_value = StringVar()
+answer_value.set("")
+
+
+def add_to_input_text(next_val):
+    current_text = input_text.get()
+    input_text.set(curent_text*next_val)
+
+
 root.mainloop()
+
