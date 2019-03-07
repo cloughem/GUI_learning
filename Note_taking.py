2e27d6d8287337fd3cf03e100808728b3a08711d
cloughem <cloughem@wgc.school.nz>
2019-03-08 10:24:52 +1300

    Note Save time

--- Note_taking.py
+++ Note_taking.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import Note
 
 root = Tk()
 root.title("Note Taker")
@@ -6,46 +7,68 @@ root.option_add("*font", "Consolas 20")
 root.geometry("400x600")
 
 
+def save_note(title, body, category="Shopping"):
+    print(title)
+    print(body)
+
+    new_note = Note(title, body, category)
+
+
+
 def open_new_note():
-    print("open new note window")
+    print("Open new note window")
+    title_value = StringVar()
+    text_value = StringVar()
     new_note_window = Toplevel(root)
 
     new_note_title = Label(new_note_window, text="New Note")
     new_note_title.grid()
 
-    title_entry = Entry(new_note_window)
-    title_entry.grid
+    title_label = Label(new_note_window, text="Title:")
+    title_label.grid(sticky=W)
+
+    title_entry = Entry(new_note_window, textvariable=title_value)
+    title_entry.grid()
 
     note_label = Label(new_note_window, text="Note text:")
-    note_label.grid()
+    note_label.grid(sticky=W)
 
-    note_entry = Entry(new_note_window)
-    note_entry.grid()
+    note_text = Text(new_note_window)
+    note_text.config(height=10, width=20)
+    note_text.grid()
+
+    button_frame = Frame(new_note_window)
+    button_frame.grid()
+
+    cancel_button = Button(button_frame, text="Cancel", command=new_note_window.destroy)
+    cancel_button.grid(row=0, column=1, sticky=E)
+
+    save_button = Button(button_frame, text="Save", command=lambda: save_note(title_value.get(), note_text.get(1.0, END)))
+    save_button.grid(row=0, column=2, sticky=E)
 
 
 def open_list(list_name):
-    print("open list")
+    print("Open a list")
 
 
 lbl_title = Label(root, text="Notes")
-lbl_title.config(font=("Calibri", "30"), width=20, bg="hot pink")
+lbl_title.config(font=("Calibri", "30"), width=20)
 lbl_title.grid(row=0, sticky=N + E + S + W)
 
-btn_newnote = Button(root, text="+New Note", command=lambda: open_new_note())
-btn_newnote.config(bg="red")
+btn_newnote = Button(root, bg="#ffa2a2", text="+ New note", command=lambda: open_new_note())
+btn_newnote.config()
 btn_newnote.grid(row=1, sticky=W)
 
-btn_shopping = Button(root, text="Shopping", command=lambda: open_list("Shopping"))
-btn_shopping.config(bg="orange")
+btn_shopping = Button(root, bg="#ffe4a2", text="Shopping", command=lambda: open_list("Shopping"))
+btn_shopping.config()
 btn_shopping.grid(row=2, sticky=E + W)
 
-btn_todo = Button(root, text="To do", command=lambda: open_list("To do"))
-btn_todo.config(bg="yellow")
+btn_todo = Button(root, bg="#f5ffa2", text="To do", command=lambda: open_list("To do"))
+btn_todo.config()
 btn_todo.grid(row=3, sticky=E + W)
 
-
-btn_hmwrk = Button(root, text="Homework", command=lambda: open_list("Homework"))
-btn_hmwrk.config(bg="lawn green")
-btn_hmwrk.grid(row=4, sticky=E + W)
+btn_homework = Button(root, text="Homework", command=lambda: open_list("Homework"))
+btn_homework.config()
+btn_homework.grid(row=4, sticky=E + W)
 
 root.mainloop()
