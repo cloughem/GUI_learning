db2c025fe77196cf7630509d551fd6cef868c6be
cloughem <cloughem@wgc.school.nz>
2019-03-15 10:05:12 +1300

    Note update again 15/3

--- Note_taking.py
+++ Note_taking.py
@@ -25,10 +25,10 @@ root.geometry("400x600")
 notes = []
 
 
-def save_note(window, title, body, category="Shopping"):
-    new_note = Note(title.title().strip(),
-                    body.title().strip(),
-                    category.title().strip())
+def save_note(window, title, body, category):
+    new_note = Note(title.capitalize().strip(),
+                    body.capitalize().strip(),
+                    category.capitalize().strip())
 
     notes.append(new_note)
     print("Category: {}".format(new_note.get_category()))
@@ -37,10 +37,9 @@ def save_note(window, title, body, category="Shopping"):
     window.destroy()
 
 
-def open_new_note():
+def open_new_note(category):
     print("Open new note window")
     title_value = StringVar()
-    text_value = StringVar()
     new_note_window = Toplevel(root)
 
     new_note_title = Label(new_note_window, text="New Note")
@@ -65,40 +64,58 @@ def open_new_note():
     cancel_button = Button(button_frame, text="Cancel", command=new_note_window.destroy)
     cancel_button.grid(row=0, column=1, sticky=E)
 
-    save_button = Button(button_frame, text="Save", command=lambda: save_note(new_note_window,
-                                                                              title_value.get(),
-                                                                              note_text.get(1.0, END)))
+    save_button = Button(button_frame, text="Save",  command=lambda: save_note(new_note_window, title_value.get(),
+                                                                               note_text.get(1.0, END), category))
+
     save_button.grid(row=0, column=2, sticky=E)
 
 
-def open_list(list_name):
-    print("Open {}".format(list_name))
+def open_list(list_category):
+    print("Open {}".format(list_category))
     list_window = Toplevel(root)
-    list_window.title(list_name)
+    list_window.geometry("600x400")
+
+    for note in notes:
+        title = note.get_title()
+        body = note.get_text()
+        category = note.get_category()
+
+        note_text = "***{}***\n{}\n".format(title, body)
+
+        if category == list_category:
+            Label(list_window, text=note_text).grid(sticky=W)
 
 
 lbl_title = Label(root, text="Notes")
 lbl_title.config(font=("Calibri", "30"), width=20)
 lbl_title.grid(row=0, sticky=N + E + S + W)
 
-btn_newnote = Button(root, bg="#ffb3b3", fg="white",  text="+ New note", command=lambda: open_new_note())
-btn_newnote.config()
-btn_newnote.grid(row=1, sticky=W)
-
 btn_shopping = Button(root, bg="#ffd9b3", fg="white",  text="Shopping", command=lambda: open_list("Shopping"))
 btn_shopping.config()
-btn_shopping.grid(row=2, sticky=E + W)
+btn_shopping.grid(row=1, sticky=E + W)
+
+btn_shopping_new = Button(root, text="+", command=lambda: open_new_note("Shopping"))
+btn_shopping_new.config()
+btn_shopping_new.grid(column=1, row=1, sticky=E + W)
 
 btn_todo = Button(root, bg="#ffffb3", fg="white",  text="To do", command=lambda: open_list("To do"))
 btn_todo.config()
-btn_todo.grid(row=3, sticky=E + W)
+btn_todo.grid(row=2, sticky=E + W)
 
 btn_homework = Button(root, bg="#d9ffb3", fg="white", text="Homework", command=lambda: open_list("Homework"))
 btn_homework.config()
-btn_homework.grid(row=4, sticky=E + W)
+btn_homework.grid(row=3, sticky=E + W)
 
 btn_but = Button(root, bg="#b3ffb3", fg="white", text="But", command=lambda: open_list("But"))
 btn_but.config()
-btn_but.grid(row=5, sticky=E + W)
+btn_but.grid(row=4, sticky=E + W)
+
+btn_todo_new = Button(root, text="+", command=lambda: open_new_note("To do"))
+btn_todo_new.config()
+btn_todo_new.grid(column=1, row=2, sticky=E + W)
+
+btn_homework_new = Button(root, text="+", command=lambda: open_new_note("Homework"))
+btn_homework_new.config()
+btn_homework_new.grid(column=1, row=3, sticky=E + W)
 
 root.mainloop()
